0aac410de1ff7088b41cdd328426ac47382064ea
Ferm0494 <ferm0494@gmail.com>
2020-08-18 14:06:03 -0600

    Make reverseString calculator(add,substract)

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const capitalize = (word) =>{
-  return word.charAt(0).toUpperCase() + word.slice(1);
+const capitalize = word => {
+    if (typeof word === 'string') {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    } else {
+        throw new Error('Not a string sent');
+    }
+};
+
+const reverseString = word => {
+    if (typeof word === 'string') {
+        let splitted = word.split("").reverse();
+        return splitted.join("");
+    } else {
+        throw new Error('Not a string sent');
+    }
+}
+
+const calculator = (a, b) => {
+    if (typeof a === 'number' && typeof b === 'number') {
+        const sum = () => a + b
+
+        const substract = () => a - b
+
+        return {
+            sum,
+            substract
+        }
+    } else {
+        throw new Error("Inputs are not numbers")
+    }
+
 }
 
 export {
-  capitalize
-}
+    capitalize,
+    reverseString,
+    calculator
+};
--- index.test.js
+++ index.test.js
@@ -1,7 +1,45 @@
 import {
-  capitalize
- } from './index';
+    capitalize,
+    reverseString,
+    calculator
+} from './index';
 
-test('hello capitalized is Hello', () =>{
-  expect(capitalize('hello')).toBe('Hello');
+test('hello capitalized is Hello', () => {
+    expect(capitalize('hello')).toBe('Hello');
 });
+
+test('capitalized a number', () => {
+    expect(() => {
+        capitalize(69)
+    }).toThrow(Error)
+})
+
+test('Reversed string', () => {
+    expect(reverseString('catdog')).toBe('godtac')
+})
+
+test('Reversed String with number input', () => {
+    expect(() => {
+        reverseString(2)
+    }).toThrow(Error)
+})
+
+test('add 2 numbers', () => {
+    expect(calculator(2, 10).sum()).toBe(12)
+})
+
+test('add 1 number and 1 string', () => {
+    expect(() => {
+        calculator(2, 'foo').sum()
+    }).toThrow(Error)
+})
+
+test('substract 2 numbers', () => {
+    expect(calculator(10, 2).substract()).toBe(8)
+})
+
+test('subtract 1 number and 1 string', () => {
+    expect(() => {
+        calculator(10, 'foo').substract()
+    }).toThrow(Error)
+})
